aae440254611db542c5d10ff3029def3310ff01c
MiguelFS <lflores.221ds15@istta.edu.pe>
2025-07-28 21:46:32 -0500

    Implementacion Models y MIgrations

---
+++ app/Models/Recipe.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'title', 'description', 'image'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function ingredients()
+    {
+        return $this->hasMany(Ingredient::class);
+    }
+
+    public function steps()
+    {
+        return $this->hasMany(Step::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Categorie::class, 'recipe_category');
+    }
+}
